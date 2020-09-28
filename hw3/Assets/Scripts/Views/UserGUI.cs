f970a8e2ba2b0af52975b70c4746b782b2092383
13421057157 <2474965565@qq.com>
2020-09-28 22:59:01 +0800

    增加hw3

---
+++ hw3/Assets/Scripts/Views/UserGUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+    IUserAction userAction;
+    public string gameMessage ;
+    public int time;
+    GUIStyle style, bigstyle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60;
+        userAction = SSDirector.GetInstance().CurrentSceneController as IUserAction;
+
+        style = new GUIStyle();
+        style.normal.textColor = Color.white;
+        style.fontSize = 30;
+
+        bigstyle = new GUIStyle();
+        bigstyle.normal.textColor = Color.white;
+        bigstyle.fontSize = 50;
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void OnGUI() {
+        userAction.Check();
+        GUI.Label(new Rect(160, Screen.height * 0.1f, 50, 200), "Preists and Devils", bigstyle);
+        GUI.Label(new Rect(250, 100, 50, 200), gameMessage, style);
+        GUI.Label(new Rect(0,0,100,50), "Time: " + time, style);
+    }
+}
