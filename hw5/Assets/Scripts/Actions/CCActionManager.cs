3c5e9d4366481aab2434e729801129a9a3c37a0a
13421057157 <2474965565@qq.com>
2020-10-20 19:14:59 +0800

    增加hw5

---
+++ hw5/Assets/Scripts/Actions/CCActionManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCActionManager : SSActionManager, IActionCallback 
+{
+    public RoundController sceneController;
+    public CCFlyAction action;
+    
+    // Start is called before the first frame update
+    protected new void Start()
+    {
+        sceneController = (RoundController)SSDirector.getInstance().currentSceneController;
+        sceneController.actionManager = this;
+    }
+
+    // Update is called once per frame
+    // protected new void Update()
+    // {
+        
+    // }
+
+    public void SSActionEvent(SSAction source,
+        SSActionEventType events = SSActionEventType.Completed,
+        int intParam = 0,
+        string strParam = null,
+        Object objectParam = null) {
+            Debug.Log("飞碟被击中或飞出视界，动作结束，飞碟被销毁");
+            if (source.transform != null) {
+                Destroy(source.transform.gameObject);
+            }
+    }
+
+    public void MoveDisk(GameObject disk) {
+        action = CCFlyAction.GetSSAction(disk.GetComponent<DiskAttributes>().speedX, disk.GetComponent<DiskAttributes>().speedY);
+        RunAction(disk, action, this);
+
+    }
+}
---
+++ hw5/Assets/Scripts/Controllers/RoundController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class RoundController : MonoBehaviour, ISceneController, IUserAction
+{
+    int round = 0;
+    int max_round = 5;
+    float timer = 0.5f;
+    GameObject disk;
+    DiskFactory factory ;
+    public CCActionManager actionManager;
+    public ScoreController scoreController;
+    public UserGUI userGUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (userGUI.mode == 0) return;
+        GetHit();
+        gameOver();
+        if (round > max_round) {
+            return;
+        }
+        timer -= Time.deltaTime;
+        if (timer <= 0 && actionManager.RemainActionCount() == 0) {
+            //从工厂中得到10个飞碟，为其加上动作
+            for (int i = 0; i < 10; ++i) {
+                disk = factory.GetDisk(round);
+                actionManager.MoveDisk(disk);
+                //Thread.Sleep(100);
+            }
+            round += 1;
+            if (round <= max_round) {
+                userGUI.round = round;
+            }
+            timer = 4.0f;
+        }
+        
+    }
+    void Awake() {
+        SSDirector director = SSDirector.getInstance();
+        director.currentSceneController = this;
+        director.currentSceneController.LoadSource();
+        gameObject.AddComponent<UserGUI>();
+        gameObject.AddComponent<CCActionManager>();
+        gameObject.AddComponent<ScoreController>();
+        gameObject.AddComponent<DiskFactory>();
+        factory = Singleton<DiskFactory>.Instance;
+        userGUI = gameObject.GetComponent<UserGUI>();
+        
+    }
+
+    public void LoadSource() 
+    {
+
+    }
+
+    public void gameOver() 
+    {
+        if (round > max_round && actionManager.RemainActionCount() == 0)
+            userGUI.gameMessage = "Game Over!";
+    }
+
+    public void GetHit() {
+        if (Input.GetButtonDown("Fire1")) {
+			// Debug.Log ("Fired Pressed");
+			// Debug.Log (Input.mousePosition);
+
+			//Vector3 mp = Input.mousePosition; //get Screen Position
+
+			//create ray, origin is camera, and direction to mousepoint
+			Camera ca = Camera.main;
+			Ray ray = ca.ScreenPointToRay(Input.mousePosition);
+
+			//Return the ray's hit
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+                scoreController.Record(hit.transform.gameObject);
+                
+				//print (hit.transform.gameObject.name);
+				// if (hit.collider.gameObject.tag.Contains("Finish")) { //plane tag
+				// 	Debug.Log ("hit " + hit.collider.gameObject.name +"!" ); 
+				// }
+                //print(hit);
+				//DestroyImmediate (hit.transform.gameObject);
+
+                Destroy (hit.transform.gameObject);
+                
+                // if (hit.transform == null) {
+                //     print("Yes");
+                // }
+                // else {
+                //     print("no");
+                //     print(hit.transform);
+                // }
+			}
+		}
+    }
+}
