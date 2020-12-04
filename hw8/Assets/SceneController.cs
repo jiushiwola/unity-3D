16afd31e628fd8bf9cbae85473aac7f3098b7230
13421057157 <2474965565@qq.com>
2020-12-04 22:47:57 +0800

    增加hw8

---
+++ hw8/Assets/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject ParticleRing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleRing = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/ParticleRing"), new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI() {
+        if (ParticleRing.GetComponent<ParticleHalo>().time >= 5) 
+        {
+            if (GUI.Button(new Rect(Screen.width/2-20,Screen.height/2-10,40,40), "+")) {
+                
+                ParticleRing.GetComponent<ParticleHalo>().minRadius = ParticleRing.GetComponent<ParticleHalo>().minRadius == 5 ? 7 : 5;
+                ParticleRing.GetComponent<ParticleHalo>().maxRadius = ParticleRing.GetComponent<ParticleHalo>().maxRadius == 12 ? 8 : 12;
+            }
+        }
+    }
+}
---
+++ hw8/README.md
@@ -0,0 +1,12 @@
+### unity 粒子光环
+#### 一、游戏介绍及实现
+[博客链接](https://blog.csdn.net/weixin_43867940/article/details/110500119)
+
+
+#### 二、视频演示
+[https://www.bilibili.com/video/bv1SK4y157LF](https://www.bilibili.com/video/bv1SK4y157LF)
+#### 三、如何运行
+- 在unity上创建一个新项目
+- 令Assets替换新项目的Assets文件夹
+- 打开项目，将Assets/Scenes下的ParticleRing场景拉到Hierarchy中，删除SampleScene
+- 点击运行
