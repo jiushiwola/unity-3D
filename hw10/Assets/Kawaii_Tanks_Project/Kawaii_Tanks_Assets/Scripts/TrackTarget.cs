6090b1d2748c96c7d29ec81155dc73303e6da14c
13421057157 <2474965565@qq.com>
2021-01-05 19:53:36 +0800

    增加hw10

---
+++ hw10/Assets/Kawaii_Tanks_Project/Kawaii_Tanks_Assets/Scripts/TrackTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TrackTarget : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent nav;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            nav.SetDestination(target.transform.position);
+        }
+    }
+}
---
+++ hw10/README.md
@@ -0,0 +1,9 @@
+### unity实现AI对战坦克
+#### 一、游戏介绍及实现
+[博客链接](https://blog.csdn.net/weixin_43867940/article/details/112250710)
+
+#### 二、如何运行
+- 在unity上创建一个新项目
+- 令Assets替换新项目的Assets文件夹
+- 打开项目，将Assets/Scenes下的AItank场景拉到Hierarchy中，删除SampleScene
+- 点击运行
